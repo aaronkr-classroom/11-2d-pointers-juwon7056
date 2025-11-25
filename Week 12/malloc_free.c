c2d8d8a7d0ab784d22f7626d7f64520967b3f334
ththfql <juwon7056@gmail.com>
2025-11-25 17:02:09 +0900

    finished

---
+++ Week 12/malloc_free.c
@@ -0,0 +1,15 @@
+﻿// malloc_free.c
+#include <stdio.h>
+
+void Test() {
+	// 힙에 200바이트 할당함
+	short* p = (short*)malloc(200);
+	// free(p); // 실수로 메모리 할당을 해제 안 하면...
+}
+
+int main(void) {
+	int i;
+	// 메모리가 100바이트씩 100번 동적 할당됨
+	for (i = 0; i < 100; i++) Test();
+	return 0;
+}
